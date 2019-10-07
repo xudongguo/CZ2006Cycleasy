dbc1963e6617bfb4d06d6c4f8f7b1c1d3189ad69
clairelin <claire.nikita.lin@gmail.com>
2019-10-07 22:48:29 +0800

    Added LoginActivity Added SignUpActivity (incomplete)

---
+++ Projects/app/src/main/java/com/example/cycleasy/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.cycleasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
