c1820e03ca777a4fc92f0c01e1f6f4b263b5dc7a
Jellymaxd <53516548+Jellymaxd@users.noreply.github.com>
2019-10-07 12:03:07 +0800

    front end for route,exercise and me page

--- Projects/app/src/main/java/com/example/cycleasy/RacksFragment.java
+++ Projects/app/src/main/java/com/example/cycleasy/RacksFragment.java
@@ -1,18 +1,45 @@
 package com.example.cycleasy;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class RacksFragment extends Fragment {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
-    @Override
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayAdapter adapter;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_racks, null);
+        View view = inflater.inflate(R.layout.fragment_racks, null);
+        final TextView searchbar=(TextView) view.findViewById((R.id.racks_searchbar));
+        searchbar.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+
+                                             //Call SearchableActivity to handle the search
+                                             Intent intent = new Intent(view.getContext(), SearchableActivity.class);
+                                             startActivity(intent);
+                                         }
+                                     });
+
+        return view;
+
+
     }
 }
