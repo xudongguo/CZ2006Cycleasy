d41c909f0c16188704c8b0ccff6a5930d9b9f2cf
clairelin <claire.nikita.lin@gmail.com>
2019-11-05 16:49:20 +0800

    Display name
    Display name

--- Projects/app/src/main/java/com/example/cycleasy/MeFragment.java
+++ Projects/app/src/main/java/com/example/cycleasy/MeFragment.java
@@ -5,17 +5,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.cycleasy.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MeFragment extends Fragment {
+/**
+ * Fragment class for activities in me page
+ */
+public class MeFragment extends Fragment{
+
+    private Fragment fragment;
+    private FirebaseAuth firebaseAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me, null);
+
+        TextView usernameText = view.findViewById(R.id.textView2);
+        String s;
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser().getDisplayName() == null) {
+            s = "Hi Guest!";
+        } else {
+            s = "Hi " + firebaseAuth.getCurrentUser().getDisplayName() + "!";
+        }
+        usernameText.setText(s);
+
+        cychisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadfragment(new subFragment_cychis());
+            }
+        });
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadfragment(new subFragment_profile());
+            }
+        });
+        favpathButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadfragment(new subFragment_favpath());
+            }
+        });
+        return view;
 
     }
 
